740c8dca1d3ffee0dff48a42ae5875cd325cd7f6
Derek Johnson <djohnson599@live.com>
2021-04-07 15:49:25 -0500

    Created baseline for mobile view on conditions

--- src/app/conditions/conditions.component.ts
+++ src/app/conditions/conditions.component.ts
@@ -1,5 +1,5 @@
 /* eslint-disable require-jsdoc */
-import {Component, OnInit} from '@angular/core';
+import {Component, HostListener, OnInit} from '@angular/core';
 import {AngularFirestore, AngularFirestoreCollection} from '@angular/fire/firestore';
 import {ThemeService} from '../theme-service/theme.service';
 import {Observable} from 'rxjs';
@@ -18,6 +18,10 @@ export class ConditionsComponent implements OnInit {
   names;
   sortType;
   config;
+  screenWidth: number;
+  isLarge: boolean;
+  opened: boolean;
+  sidenavMode: string;
 
   constructor(private afs: AngularFirestore, private themeService: ThemeService, private fns: AngularFireFunctions) {
     this.themeService.initTheme();
@@ -32,40 +36,66 @@ export class ConditionsComponent implements OnInit {
      this.isDarkMode ? this.themeService.updateTheme('custom-light-theme') : this.themeService.updateTheme('custom-dark-theme');
    }
 
-   ngOnInit(): void {
-     const callable = this.fns.httpsCallable('getSecureKey');
-     this.sortType = 'pathfinderConditions';
-     callable({restrictedIndex: this.sortType}).subscribe((data) => {
-       this.config = {
-         apiKey: data.key,
-         appId: 'HJN7F66MDX',
-         indexName: this.sortType,
-         routing: true,
-       };
-       this.searchLoaded = Promise.resolve(true);
-     });
-     this.conditionsCollection = this.afs.collection('conditions');
-     this.conditions = this.conditionsCollection.valueChanges();
+   /** @function
+     * @name checkWidth
+     * @listens window:resize
+     * @hostListener
+     * @description checkWidth listens to window resize and adjusts the isLarge Boolean.
+     */
+  @HostListener('window:resize') checkWidth() {
+     //  alert('it works!');
+     this.screenWidth = window.innerWidth;
+     if (this.screenWidth <= 600) {
+       this.isLarge = false;
+     } else {
+       this.isLarge = true;
+     }
    }
 
-   addData(): void {
-     data.forEach((element) => {
-       const addable = {
-         name: '',
-         text: '',
-         source: '',
-       };
-       if (element.name) {
-         addable.name = element.name;
-       }
-       if (element.text) {
-         addable.text = element.text[0];
-       }
-       if (element.source) {
-         addable.source = element.source;
-       }
-       this.conditionsCollection.add(addable);
-       console.log('Condition Added');
-     });
-   }
+  ngOnInit(): void {
+    this.screenWidth = window.innerWidth;
+    if (this.screenWidth <= 600) {
+      this.isLarge = false;
+      this.opened = false;
+      this.sidenavMode = 'over';
+    } else {
+      this.isLarge = true;
+      this.opened = true;
+      this.sidenavMode = 'side';
+    }
+    const callable = this.fns.httpsCallable('getSecureKey');
+    this.sortType = 'pathfinderConditions';
+    callable({restrictedIndex: this.sortType}).subscribe((data) => {
+      this.config = {
+        apiKey: data.key,
+        appId: 'HJN7F66MDX',
+        indexName: this.sortType,
+        routing: true,
+      };
+      this.searchLoaded = Promise.resolve(true);
+    });
+    this.conditionsCollection = this.afs.collection('conditions');
+    this.conditions = this.conditionsCollection.valueChanges();
+  }
+
+  addData(): void {
+    data.forEach((element) => {
+      const addable = {
+        name: '',
+        text: '',
+        source: '',
+      };
+      if (element.name) {
+        addable.name = element.name;
+      }
+      if (element.text) {
+        addable.text = element.text[0];
+      }
+      if (element.source) {
+        addable.source = element.source;
+      }
+      this.conditionsCollection.add(addable);
+      console.log('Condition Added');
+    });
+  }
 }
